e8ed44aced342fedc948d4c60389c576733a825b
zazin <nurza.cool@gmail.com>
2016-09-17 02:14:10 +0700

    Update Baseconfig.php

--- Baseconfig.php
+++ Baseconfig.php
@@ -1,13 +1,17 @@
 <?php 
 class Baseconfig {
   public $var = '';
+	public function init()
+	{
+		require_once __DIR__ .'/vendor/autoload.php';
+	}
 	public function get()
 	{
 		return $this->var;
 	}
 	public function set($newVar)
 	{
-	  $this->var=$newVar;
+	  	$this->var=$newVar;
 		return $this->var;
 	}
 }
